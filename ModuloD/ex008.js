261b0989cab0dcb1571902353fcb83cbc45699bd
ScalcoHenrique <65136408+ScalcoHenrique@users.noreply.github.com>
2020-07-25 17:08:35 -0300

    Fim da aula 11
    Já feito o exercício solicitado

---
+++ ModuloD/ex008.js
@@ -0,0 +1,9 @@
+//Em arquivos JS puro, para escrever na tela, usar o seguinte comando:
+//console.log('Olá, Mundo!')
+
+var velocidade = 60
+console.log(`A velocidade do seu carro é de ${velocidade} Km/h.`)
+if (velocidade > 60) { //Condição Simples!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança.`)
